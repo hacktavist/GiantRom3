6f2a27155180286af9b628a0b8ec7ce0e35434f2
hacktavist <hacktavist616@gmail.com>
2016-08-16 01:35:50 -0400

    lets try building a mac player instead

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,14 +3,14 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="PierceRealityAutoBuild"
 
-echo 'Attempting to build $project for Windows'
+echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 

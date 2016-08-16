ad04f2250e4be93c696863655f82b6643c620237
hacktavist <hacktavist616@gmail.com>
2016-08-15 23:05:26 -0400

    fixing install script and reverting .travis.yml

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,9 +1,30 @@
+
 #! /bin/sh
 
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=a6d8d714de6f
+VERSION=5.4.0f3
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: #http://unity3d.com/get-unity/download/archive
-echo 'DL'
-curl -o Unity.pkg download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"

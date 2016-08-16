7d627fa454b4b01063a2f3d4061fad82b339e4c4
hacktavist <hacktavist616@gmail.com>
2016-08-16 03:07:16 -0400

    changing unity cli build settings

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,14 +3,12 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="PierceRealityAutoBuild"
 
-echo "Attempting to build $project for OS X"
+echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
-  #-nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 

7340c97ecf82b189ede7deee04b54ca166d03f46
hacktavist <hacktavist616@gmail.com>
2016-08-16 00:13:38 -0400

    silo room v2 had a merge conflict that slipped by; hopefully this is the build error I got

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,35 +5,36 @@ project="PierceRealityAutoBuild"
 
 echo 'Attempting to build $project for Windows'
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#C:/Program\ Files/Unity/Editor/Unity.exe \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
+  -projectPath "$(pwd)" \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
-
-#FILENAME="PierceRealityWindowsBuild"
-#mv $(pwd)/Build/windows/$project.exe $FILENAME
-#mv $(pwd)/Build/windows/$project_Data $FILENAME
-#zip -r $FILENAME.zip
+sleep 1m
 
 
+FILENAME="$(pwd)PierceRealityWindowsBuild"
+mv $(pwd)/Build/windows/$project.exe $FILENAME
+mv $(pwd)/Build/windows/$project_Data $FILENAME
+zip -r $FILENAME.zip
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
-#echo "Deploy to Itch"
-#wget http://dl.itch.ovh/butler/linux-amd64/head/butler
-#chmod +x butler
-#touch butler_creds
-#echo -n $ITCH_API_KEY > butler_creds
+echo "Deploy to Itch"
+wget http://dl.itch.ovh/butler/linux-amd64/head/butler
+chmod +x butler
+touch butler_creds
+echo -n $ITCH_API_KEY > butler_creds
 
-#./butler push $FILENAME.zip hacktavist/testing-travisci-deployment
+./butler push $FILENAME.zip hacktavist/testing-travisci-deployment
 
-#echo "Cleaning"
+echo "Cleaning"
 
-#./butler logout -i butler_creds --assume-yes
+./butler logout -i butler_creds --assume-yes
 #rm butler
